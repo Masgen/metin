baa233c9ba9800b4d5a39c4492c3c125dc81e9e4
Mistral Contrastin <madgenhetic@gmail.com>
2012-08-16 19:39:14 +0300

    migration fix

--- db/migrate/20120517094918_create_stories.rb
+++ db/migrate/20120517094918_create_stories.rb
@@ -1,14 +1,15 @@
 class CreateStories < ActiveRecord::Migration
   def change
-    create_table :stories do |t|
-      t.text :content
-      t.string :video
-      t.references :album
-      t.string :title
-      t.datetime :event_date
-
-      t.timestamps
-    end
-    add_index :stories
+    #create_table :stories do |t|
+    #  t.text :content
+    #  t.string :video
+    #  t.references :album
+    #  t.string :title
+    #  t.datetime :event_date
+    #
+    #  t.timestamps
+    #end
+    add_index :stories, :content
+    add_index :stories, :title
   end
 end
